d927c4e3d258182ecc80f79f2c08f4462b0e8687
Sagar Deshpande <deshpandesagar1704@gmail.com>
2016-02-23 01:01:02 -0600

    Initial Commit.

---
+++ src/bigdata/sea/reducejoin/AverageAgeApplication.java
@@ -0,0 +1,17 @@
+package bigdata.sea.reducejoin;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+
+public class AverageAgeApplication extends Configured implements Tool {
+
+	public static void main(String[] args) {
+
+	}
+
+	@Override
+	public int run(String[] args) throws Exception {
+		return 0;
+	}
+
+}
